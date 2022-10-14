0bfcc9b218e74332e56f2da27d5c2812a75095e8
thielzhang <thielzhang@tencent.com>
2022-10-14 12:53:48 +0800

    convert exgameplay plugin to submodule

--- Plugins/ExGameplayPlugin/Source/ExGameplayLibrary/Private/FileHelperLibrary.cpp
+++
@@ -1,6 +0,0 @@
-#include "FileHelperLibrary.h"
-
-bool UFileHelperLibrary::LoadFileToArray(TArray<uint8>& Result, const FString& FileName, int Flags)
-{
-	return FFileHelper::LoadFileToArray(Result, *FileName, Flags);
-}
