865c47fa62abb3ae5da073aa43c5cc5120e8fbe4
thielzhang <thielzhang@tencent.com>
2023-04-13 16:28:03 +0800

    add mass

--- Tools/InitProject.bat
+++ Tools/InitProject.bat
@@ -1,5 +1,8 @@
 set EditorPath=D:/Workspace/UnrealEngine
 set ToolsPath=%~dp0
 
-mkdir %ToolsPath%..\Content\Characters\
-cp %EditorPath%/Templates/TemplateResources/High/Characters/Content/* ../Content/Characters/ -r
+rem mkdir %ToolsPath%..\Content\Characters\
+rem cp %EditorPath%/Templates/TemplateResources/High/Characters/Content/* ../Content/Characters/ -r
+
+mkdir %ToolsPath%..\Content\LevelPrototyping\
+cp %EditorPath%/Templates/TemplateResources/High/LevelPrototyping/Content/* ../Content/LevelPrototyping/ -r
