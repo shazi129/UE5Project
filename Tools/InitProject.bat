03edca131c1a09c39d80eb99049a208b981c08c5
thielzhang <thielzhang@tencent.com>
2023-04-14 10:13:03 +0800

    Add EQS Sample

--- Tools/InitProject.bat
+++ Tools/InitProject.bat
@@ -4,5 +4,9 @@ set ToolsPath=%~dp0
 rem mkdir %ToolsPath%..\Content\Characters\
 rem cp %EditorPath%/Templates/TemplateResources/High/Characters/Content/* ../Content/Characters/ -r
 
-mkdir %ToolsPath%..\Content\LevelPrototyping\
-cp %EditorPath%/Templates/TemplateResources/High/LevelPrototyping/Content/* ../Content/LevelPrototyping/ -r
+rem mkdir %ToolsPath%..\Content\LevelPrototyping\
+rem cp %EditorPath%/Templates/TemplateResources/High/LevelPrototyping/Content/* ../Content/LevelPrototyping/ -r
+
+mkdir %ToolsPath%..\Content\ThirdPerson\
+cp %EditorPath%/Templates/TP_ThirdPersonBP/Content/ThirdPerson/Blueprints ../Content/ThirdPerson/ -r
+cp %EditorPath%/Templates/TP_ThirdPersonBP/Content/ThirdPerson/Input ../Content/ThirdPerson/ -r
