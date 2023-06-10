775fc381b60c7bbab9162ca535825ca92438eac1
zhangwen <apply_count@sina.com>
2023-06-10 21:06:29 +0800

    纠正错误

--- Tools/InitProject.bat
+++ Tools/InitProject.bat
@@ -1,11 +1,11 @@
 set EditorPath=D:/Workspace/UnrealEngine
 set ToolsPath=%~dp0
 
-rem mkdir %ToolsPath%..\Content\Characters\
-rem cp %EditorPath%/Templates/TemplateResources/High/Characters/Content/* ../Content/Characters/ -r
+mkdir %ToolsPath%..\Content\Characters\
+cp %EditorPath%/Templates/TemplateResources/High/Characters/Content/* ../Content/Characters/ -r
 
-rem mkdir %ToolsPath%..\Content\LevelPrototyping\
-rem cp %EditorPath%/Templates/TemplateResources/High/LevelPrototyping/Content/* ../Content/LevelPrototyping/ -r
+mkdir %ToolsPath%..\Content\LevelPrototyping\
+cp %EditorPath%/Templates/TemplateResources/High/LevelPrototyping/Content/* ../Content/LevelPrototyping/ -r
 
 mkdir %ToolsPath%..\Content\ThirdPerson\
 cp %EditorPath%/Templates/TP_ThirdPersonBP/Content/ThirdPerson/Blueprints ../Content/ThirdPerson/ -r
