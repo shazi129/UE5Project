aa9281e5dd91c105cdbf4f6dc877f7e4217916c1
thielzhang <thielzhang@tencent.com>
2023-08-06 11:13:58 +0800

    同步

--- Tools/InitProject.bat
+++ Tools/InitProject.bat
@@ -1,12 +1,13 @@
-set EditorPath=C:\MetaWorld\UE5
+setx UE_Path D:\Workspace\UnrealEngine
+
 set ToolsPath=%~dp0
 
 mkdir %ToolsPath%..\Content\Characters\
-cp %EditorPath%/Templates/TemplateResources/High/Characters/Content/* ../Content/Characters/ -r
+cp %UE_Path%/Templates/TemplateResources/High/Characters/Content/* ../Content/Characters/ -r
 
 mkdir %ToolsPath%..\Content\LevelPrototyping\
-cp %EditorPath%/Templates/TemplateResources/High/LevelPrototyping/Content/* ../Content/LevelPrototyping/ -r
+cp %UE_Path%/Templates/TemplateResources/High/LevelPrototyping/Content/* ../Content/LevelPrototyping/ -r
 
 mkdir %ToolsPath%..\Content\ThirdPerson\
-cp %EditorPath%/Templates/TP_ThirdPersonBP/Content/ThirdPerson/Blueprints ../Content/ThirdPerson/ -r
-cp %EditorPath%/Templates/TP_ThirdPersonBP/Content/ThirdPerson/Input ../Content/ThirdPerson/ -r
+cp %UE_Path%/Templates/TP_ThirdPersonBP/Content/ThirdPerson/Blueprints ../Content/ThirdPerson/ -r
+cp %UE_Path%/Templates/TP_ThirdPersonBP/Content/ThirdPerson/Input ../Content/ThirdPerson/ -r
